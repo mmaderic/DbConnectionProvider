5b53ef81aed311b2227e8319925b5ddd67010733
Mateo Mađerić <mateo.maderic@gmail.com>
2020-01-05 23:53:12 +0100

    added IDbTransactionManager interface

---
+++ DbConnectionProvider.Abstractions/IDbTransactionManager.cs
@@ -0,0 +1,11 @@
+﻿
+namespace DbConnectionProvider.Abstractions
+{
+    public interface IDbTransactionManager
+    {
+        public bool TrySetTransactionOwner();
+        public void CommitTransaction();
+        public void RollbackTransaction();
+        public void ConcludeTransaction(bool commit);
+    }
+}
