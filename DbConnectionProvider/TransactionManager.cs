c44f4efdd7db8f22e078b8b32ec9c4eef57554c5
Mateo Mađerić <mateo.maderic@gmail.com>
2020-01-18 00:35:52 +0100

    transaction can be locked by key

--- DbConnectionProvider/TransactionManager.cs
+++ DbConnectionProvider/TransactionManager.cs
@@ -7,40 +7,81 @@ namespace DbConnectionProvider
 {
     public class TransactionManager : IDbTransactionManager
     {
-        private readonly IEnumerable<IDbTransactionProvider> _transactionProviders;
-        private bool _transactionOwnerSet;
-        private bool _rollBackTransaction;
+        private readonly IEnumerable<IDbConnectionProvider> _connectionProviders;
+        private Guid? _ownerID;       
 
         public TransactionManager(IServiceProvider servicesProvider) =>
-            _transactionProviders = servicesProvider.GetServices<IDbTransactionProvider>();
+            _connectionProviders = servicesProvider.GetServices<IDbConnectionProvider>();
+
+        public void SetTransactionOwner(Guid ownerID)
+        {
+            if (!(_ownerID is null))
+                throw new InvalidOperationException("Transaction owner is already set.");
+
+            _ownerID = ownerID;
+        }
 
         public bool TrySetTransactionOwner()
         {
-            if (_transactionOwnerSet)
+            if (!(_ownerID is null))
                 return false;
 
-            _transactionOwnerSet = true;
+            _ownerID = Guid.Empty;
             return true;
+        }
+
+        public bool TrySetTransactionOwner(Guid ownerID)
+        {
+            if (!(_ownerID is null))
+                return false;
+
+            _ownerID = ownerID;
+            return true;
+        }
+
+        public void CommitTransaction(Guid ownerID)
+        {
+            if (ownerID != _ownerID)
+                throw new InvalidOperationException("Wrong transaction owner ID provided.");
+
+            foreach (var provider in _connectionProviders)
+                provider.CommitTransaction();
+        }
+
+        public void RollbackTransaction(Guid ownerID)
+        {
+            if (ownerID != _ownerID)
+                throw new InvalidOperationException("Wrong transaction owner ID provided.");
+
+            foreach (var provider in _connectionProviders)
+                provider.RollbackTransaction();
         }
 
         public void CommitTransaction()
         {
-            if (_rollBackTransaction)
-                return;
+            if (_ownerID != Guid.Empty)
+                throw new InvalidOperationException("Transaction is locked with ownerID");
 
-            foreach(var provider in _transactionProviders)
+            foreach (var provider in _connectionProviders)
                 provider.CommitTransaction();
         }
 
         public void RollbackTransaction()
         {
-            if (_rollBackTransaction)
-                return;
+            if (_ownerID != Guid.Empty)
+                throw new InvalidOperationException("Transaction is locked with ownerID");
 
-            foreach (var provider in _transactionProviders)
+            foreach (var provider in _connectionProviders)
                 provider.RollbackTransaction();
+        }
 
-            _rollBackTransaction = true;
+        public void ConcludeTransaction(bool commit, Guid ownerID)
+        {
+            if (commit)
+                CommitTransaction(ownerID);
+            else RollbackTransaction(ownerID);
+
+            _ownerID = null;
         }
 
         public void ConcludeTransaction(bool commit)
@@ -49,7 +90,7 @@ namespace DbConnectionProvider
                 CommitTransaction();
             else RollbackTransaction();
 
-            _transactionOwnerSet = false;
+            _ownerID = null;
         }
     }
 }
