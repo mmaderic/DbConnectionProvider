ba3c7837090c5983eef548da1434960b45b68f1f
Mateo Mađerić <mateo.maderic@gmail.com>
2020-01-18 00:33:51 +0100

    connection provider and transaction provider simplified into single provider

--- DbConnectionProvider/ConnectionProvider.cs
+++ DbConnectionProvider/ConnectionProvider.cs
@@ -4,16 +4,24 @@ using System.Data;
 
 namespace DbConnectionProvider
 {
-    public class ConnectionProvider<TConnection> : IDbConnectionProvider<TConnection>, IDisposable
+    public class ConnectionProvider<TConnection, TTransaction> : IDbConnectionProvider<TConnection, TTransaction>, IDisposable
         where TConnection : IDbConnection, new()
+        where TTransaction : IDbTransaction
     {
         private readonly object lockObject = new object();
-        private readonly string _connectionString;
+        private readonly string _connectionString;      
         private TConnection _connection;
+        private TTransaction _transaction;
 
-        public ConnectionProvider(string connectionString) => _connectionString = connectionString;
+        public string Identifier { get; }
 
-        public TConnection Provide()
+        public ConnectionProvider(string connectionString) 
+            => _connectionString = connectionString;
+
+        public ConnectionProvider(string connectionString, string identifier) : this(connectionString)
+            => Identifier = identifier;
+
+        public TConnection ProvideConnection()
         {
             lock (lockObject)
             {
@@ -27,12 +35,53 @@ namespace DbConnectionProvider
             }
         }
 
-        public void Close() => Dispose();
+        public TTransaction ProvideTransaction()
+        {
+            lock (lockObject)
+            {
+                if(_transaction is null)                
+                    _transaction = (TTransaction)_connection.BeginTransaction();     
+
+                return _transaction;
+            }
+        }
+
+        public void CommitTransaction()
+        {
+            lock (lockObject)
+            {
+                _transaction?.Commit();
+                DisposeTransaction();
+            }
+        }
+
+        public void RollbackTransaction()
+        {
+            lock (lockObject)
+            {
+                _transaction?.Rollback();
+                DisposeTransaction();
+            }
+        }
+
+        public void CloseConnection() => Dispose();
+
+        public void DisposeTransaction()
+        {
+            _transaction?.Dispose();
+
+            _transaction = default;
+        }
 
         public void Dispose()
         {
             lock (lockObject)
             {
+                _transaction?.Rollback();
+                _transaction?.Dispose();
+
+                _transaction = default;
+
                 _connection?.Close();
                 _connection?.Dispose();
 
