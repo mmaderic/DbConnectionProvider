1fb39a34098a376f1eb0b0e49644857ac2d65594
Mateo Mađerić <mateo.maderic@gmail.com>
2020-01-18 00:35:11 +0100

    implemented connection resolver by identifier when multiple connection providers with equal type signatures are registered

---
+++ DbConnectionProvider/ConnectionResolver.cs
@@ -0,0 +1,34 @@
+﻿using DbConnectionProvider.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace DbConnectionProvider
+{
+    public class ConnectionResolver : IDbConnectionResolver
+    {
+        private readonly IEnumerable<IDbConnectionProvider> _connectionProviders;
+
+        public ConnectionResolver(IServiceProvider servicesProvider) =>
+            _connectionProviders = servicesProvider.GetServices<IDbConnectionProvider>();
+
+        public IDbConnectionProvider ResolveFor(string identifier)
+            => _connectionProviders.Single(x => x.Identifier == identifier);
+
+        public IDbConnectionProvider<TConnection, TTransaction> ResolveFor<TConnection, TTransaction>(string identifier)
+            where TConnection : IDbConnection
+            where TTransaction : IDbTransaction
+            => (IDbConnectionProvider<TConnection, TTransaction>) _connectionProviders.Single(x => x.Identifier == identifier);
+
+        public void CloseConnectionFor(string identifier)
+            => _connectionProviders.Single(x => x.Identifier == identifier).CloseConnection();
+
+        public void CloseAllConnections()
+        {
+            foreach (var provider in _connectionProviders)
+                provider.CloseConnection();
+        }        
+    }
+}
