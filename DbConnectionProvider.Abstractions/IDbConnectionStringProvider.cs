e01a68de71d5ca17356f43ceb7823cef3ebb3d9e
Mateo Mađerić <mateo.maderic@gmail.com>
2020-01-19 14:23:31 +0100

    Added data source count to connection string provider interface

--- DbConnectionProvider.Abstractions/IDbConnectionStringProvider.cs
+++ DbConnectionProvider.Abstractions/IDbConnectionStringProvider.cs
@@ -4,5 +4,6 @@ namespace DbConnectionProvider.Abstractions
     public interface IDbConnectionStringProvider
     {
         public string ProvideFor(string databaseName);
+        public int DataSourceCount();
     }
 }
