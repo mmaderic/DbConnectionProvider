823e51e8d5af60ee0b66e974e466480176601f51
Mateo Mađerić <mateo.maderic@gmail.com>
2020-02-02 11:57:35 +0100

    added sql server connection provider

---
+++ DbConnectionProvider.SqlServer/ServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using DbConnectionProvider.Abstractions;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+
+namespace DbConnectionProvider.SqlServer
+{
+    public static class ServiceCollectionExtensions
+    {   
+        public static IServiceCollection AddSqlConnectionProvider(this IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            var connectionStrings = configuration.ReadConnectionStrings();
+
+            serviceCollection.AddSingleton<IDbConnectionStringProvider, ConnectionStringProvider>(
+               x => new ConnectionStringProvider(connectionStrings));
+
+            foreach (var connection in connectionStrings)
+            {
+                serviceCollection.AddScoped<IDbConnectionProvider<SqlConnection, SqlTransaction>>(
+                    x => new SqlConnectionProvider(connection.ConnectionString, connection.Identifier));
+
+                serviceCollection.AddScoped<IDbConnectionProvider>(x => x.GetServices<IDbConnectionProvider<SqlConnection, SqlTransaction>>()
+                    .Single(x => x.Identifier == connection.Identifier));
+            }
+
+            serviceCollection.AddScoped<IDbConnectionResolver, ConnectionResolver>();
+            serviceCollection.AddScoped<IDbTransactionManager, TransactionManager>();
+
+            return serviceCollection;
+        }
+    }
+}
---
+++ DbConnectionProvider.SqlServer/SqlConnectionProvider.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace DbConnectionProvider.SqlServer
+{
+    public class SqlConnectionProvider : ConnectionProvider<SqlConnection, SqlTransaction>
+    {
+        public SqlConnectionProvider(string connectionString) : base(connectionString)
+        {
+        }
+
+        public SqlConnectionProvider(string connectionString, string identifier) : base(connectionString, identifier)
+        {
+        }
+    }
+}
